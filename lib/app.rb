d6c5f008398ca257c68b976a3b43d7007bb52068
Jacek Szlachta <jacek.szlachta@gmail.com>
2016-09-11 08:58:31 +0100

    added LHR airport

--- lib/app.rb
+++ lib/app.rb
@@ -5,7 +5,7 @@ require_relative "radar"
 
 class FlightSchool < Sinatra::Base
   get "/" do
-    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "LAX"] }
+    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "LAX", "LHR"] }
   end
 
   get "/airports/:code" do
